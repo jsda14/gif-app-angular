f47ddcd457b1baa0c3c36fedecd70684f7f08301
jsda14 <jsda14@gmail.com>
2022-09-19 10:57:37 -0500

    gifs-app

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+
+  get historial (){
+    return this.gifsServices.historial;
+  }
+  constructor(private gifsServices: GifsService) { }
+
+  buscar(termino:string ){
+   this.gifsServices.buscarGifs(termino);
+  }
+
+}
